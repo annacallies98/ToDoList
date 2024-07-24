a3ee18a62f85dc88df0ce94e0f732a4b8a02ecb1
Anna Callies <annacallies98@gmail.com>
2024-07-24 11:01:58 -0500

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Anna Callies on 7/24/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
